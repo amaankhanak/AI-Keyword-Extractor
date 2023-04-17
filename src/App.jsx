f6337028ca33a914b6c2b495fd737f05f5da073b
amaankhanakz <sameeramaan2014@gmail.com>
2023-04-17 22:42:28 +0530

    ui done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TextInput from "./components/TextInput";
+
+const App = () => {
+  const extractKeywords = (text) => {
+    console.log(text);
+  };
+
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxWidth="3xl" centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+    </Box>
+  );
+};
+
+export default App;
+// 23:03
